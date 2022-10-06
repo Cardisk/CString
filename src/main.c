5fc1b33a8f68a3a02b501a2d9df4c8d6b19c429d
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2022-10-06 14:13:55 +0200

    chars renamed to_chars modified some methods

--- src/main.c
+++ src/main.c
@@ -3,42 +3,42 @@
 
 int main() {
     String s1 = create("abc");
-    printf("CHARS: %s\n", chars(s1));
+    printf("CHARS: %s\n", to_chars(s1));
 
     set(&s1, "HelLO");
-    printf("CASEFOLD: %s -> %s\n", chars(s1), casefold(s1));
+    printf("CASEFOLD: %s -> %s\n", to_chars(s1), casefold(s1));
 
     set(&s1, "test");
-    printf("CAPITALIZE: %s -> %s\n", chars(s1), capitalize(s1));
+    printf("CAPITALIZE: %s -> %s\n", to_chars(s1), capitalize(s1));
 
-    printf("UPPER: %s -> %s\n", chars(s1), upper(s1));
+    printf("UPPER: %s -> %s\n", to_chars(s1), upper(s1));
 
     String s2 = create("aaa");
-    printf("COMPARE: %s, %s -> %d\n", chars(s1), chars(s2), compare(s1, s2));
+    printf("COMPARE: %s, %s -> %d\n", to_chars(s1), to_chars(s2), compare(s1, s2));
 
     set(&s2, "concat");
-    printf("CONCAT: %s, %s -> %s\n", chars(s1), chars(s2), concat(s1, s2));
+    printf("CONCAT: %s, %s -> %s\n", to_chars(s1), to_chars(s2), concat(s1, s2));
 
-    printf("ISEMPTY: %s -> %d\n", chars(s1), isEmpty(s1));
+    printf("ISEMPTY: %s -> %d\n", to_chars(s1), isEmpty(s1));
 
-    printf("INDEXOF: %s, %c -> %d\n", chars(s1), 's', indexOf(s1, 's'));
+    printf("INDEXOF: %s, %c -> %d\n", to_chars(s1), 's', indexOf(s1, 's'));
 
-    printf("INDEXOFSUBSTR: %s, %s -> %d\n", chars(s1), "es", indexOfSubStr(s1, "es"));
+    printf("INDEXOFSUBSTR: %s, %s -> %d\n", to_chars(s1), "es", indexOfSubStr(s1, "es"));
 
-    printf("CONTAINS: %s, %s -> %d\n", chars(s1), "t", contains(s1, "t"));
+    printf("CONTAINS: %s, %s -> %d\n", to_chars(s1), "t", contains(s1, "t"));
 
-    printf("ENDSWITH: %s, %s -> %d\n", chars(s1), "st", endsWith(s1, "st"));
+    printf("ENDSWITH: %s, %s -> %d\n", to_chars(s1), "st", endsWith(s1, "st"));
 
-    printf("STARTSWITH: %s, %s -> %d\n", chars(s1), "t", startsWith(s1, "t"));
+    printf("STARTSWITH: %s, %s -> %d\n", to_chars(s1), "t", startsWith(s1, "t"));
 
-    printf("EQUALS: %s, %s -> %d\n", chars(s1), chars(s2), equals(s1, s2));
+    printf("EQUALS: %s, %s -> %d\n", to_chars(s1), to_chars(s2), equals(s1, s2));
 
-    printf("MATCHES: %s, %s -> %d\n", chars(s1), "[0-9]", matches(s1, "[0-9]"));
+    printf("MATCHES: %s, %s -> %d\n", to_chars(s1), "[0-9]", matches(s1, "[0-9]"));
 
-    printf("REPLACE: %s, %c, %c -> %s\n", chars(s1), 's', 'S', replace(s1, 's', 'S'));
+    printf("REPLACE: %s, %c, %c -> %s\n", to_chars(s1), 's', 'S', replace(s1, 's', 'S'));
 
     set(&s1, "  test  ");
-    printf("TRIM: \'%s\' -> \'%s\'\n", chars(s1), trim(s1));
+    printf("TRIM: \'%s\' -> \'%s\'\n", to_chars(s1), trim(s1));
 
     destroy(&s1);
     destroy(&s2);
